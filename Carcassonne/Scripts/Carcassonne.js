fa607c992e9a6872a8bc702c4d8693e0fd341549
Jared Barden <jdbarden@gmail.com>
2013-05-30 23:22:38 -0400

    Failing at jQuery

--- Carcassonne/Scripts/Carcassonne.js
+++ Carcassonne/Scripts/Carcassonne.js
@@ -1,4 +1,29 @@
 $(document).ready(function(){
 
-	$(#
+    $("#SubmitPlayersButton").click(StartGame);
+
+    $("#CreateGameButton").on("click", CreateGame);
 });
+
+function StartGame() {
+    alert("here");
+    alert($("[id*=PlayerColor]").val());
+}
+
+function CreateGame() {
+
+    StartGame();
+
+    $.ajax(
+        {
+            url: "/Game/Create",
+            data:
+                {
+                    Players: 4
+                },
+            success: function (result) {
+                $("#PlayerInformation").html(result);
+            }
+        }
+        );
+}
